7e7340bc85a21ef8d77242e2a6113445da592dda
QUOCCUONG189 <maiq70933@gmail.com>
2026-01-19 10:00:23 +0700

    Update image_fetcher.py

--- image_fetcher.py
+++ image_fetcher.py
@@ -1,29 +1,17 @@
-import requests
-import os
-from dotenv import load_dotenv
+import os, requests
 
-load_dotenv()
-UNSPLASH_KEY = os.getenv("UNSPLASH_ACCESS_KEY")
-
-def fetch_image(query, idx):
+def get_image(query):
+    key = os.environ["UNSPLASH_ACCESS_KEY"]
     url = "https://api.unsplash.com/search/photos"
-    params = {
+
+    r = requests.get(url, params={
         "query": query,
         "per_page": 1
-    }
-    headers = {
-        "Authorization": f"Client-ID {UNSPLASH_KEY}"
-    }
+    }, headers={
+        "Authorization": f"Client-ID {key}"
+    })
 
-    r = requests.get(url, params=params, headers=headers)
     data = r.json()
-
     if data["results"]:
-        img_url = data["results"][0]["urls"]["regular"]
-        img_data = requests.get(img_url).content
-        path = f"image_{idx}.jpg"
-        with open(path, "wb") as f:
-            f.write(img_data)
-        return path
-
+        return data["results"][0]["urls"]["regular"]
     return None
